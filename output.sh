18761a991ee6f9dac9dcac916b21895974826f72
Tim Lamborn <tlamborn@gmail.com>
2026-03-23 23:01:09 +1300

    Update output scripts

--- output.sh
+++ output.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-python3 "scripts/output.py" -l 4 --format jlc --gif --gif-duration 5
+python3 "scripts/output.py" -l 4 --format jlc --anim-format webp
--- scripts/image.py
+++ scripts/image.py
@@ -1,4 +1,27 @@
 from PIL import Image
+import subprocess, os, shutil
+
+IMAGE_BACKENDS = {
+    ".gif": "pil",
+    ".webp": "pil",
+    ".mp4": "ffmpeg",
+    ".webm": "ffmpeg",
+}
+
+ANIMATION_FORMATS = [k[1:] for k in IMAGE_BACKENDS.keys()]
+
+def get_extn(path: str):
+    return os.path.splitext(path)[1]
+
+def get_backend(format: str):
+    format = f".{format}"
+    if not format in IMAGE_BACKENDS:
+        return None
+    backend = IMAGE_BACKENDS[format]
+    if backend == "ffmpeg":
+        if not shutil.which("ffmpeg"):
+            return None
+    return backend
 
 def crop_image(src: str, dst: str):
     img = Image.open(src)
@@ -14,13 +37,15 @@ def crop_image(src: str, dst: str):
     
     img.crop(bbox).save(dst)
 
-
-def make_gif(sources: list[str], dst: str, framerate: int = 10):
-    images = [ Image.open(path).convert("RGBA") for path in sources ]
-    
+def find_bounding_box(images: list[Image.Image|str] ):
     # Work out the worst case bounding box
     union_bbox = None
     for img in images:
+
+        # Images may be PIL image or path.
+        if type(img) is str:
+            img = Image.open(img).convert("RGBA")
+
         alpha = img.getchannel("A")
         bbox = alpha.getbbox()
         if bbox:
@@ -36,9 +61,20 @@ def make_gif(sources: list[str], dst: str, framerate: int = 10):
     
     if not union_bbox:
         raise Exception("All frames are fully transparent!")
-    
-    images = [img.crop(union_bbox) for img in images]
+    return union_bbox
 
+
+def make_animation(sources: list[str], dst: str, framerate: int = 10):
+    bbox = find_bounding_box(sources)
+
+    backend = IMAGE_BACKENDS[get_extn(dst)]
+    if backend == "pil":
+        make_animation_pil(sources, dst, framerate, bbox)
+    elif backend == "ffmpeg":
+        make_animation_ffmpeg(sources, dst, framerate, bbox)
+
+def make_animation_pil(sources: list[str], dst: str, framerate: int, bbox: list[int]):
+    images = [ Image.open(path).convert("RGBA").crop(bbox) for path in sources ]
     images[0].save(
         dst,
         save_all=True,
@@ -47,3 +83,25 @@ def make_gif(sources: list[str], dst: str, framerate: int = 10):
         loop=0,
         disposal=2
     )
+
+def make_animation_ffmpeg(sources: list[str], dst: str, framerate: int, bbox: list[int]):
+    x0, y0, x1, y1 = bbox
+
+    frame_info_path = os.path.join(os.path.dirname(dst), "ffmpeg-frames.txt")
+    with open(frame_info_path, 'w') as f:
+        for path in sources:
+            f.write(f"file '{os.path.abspath(path)}'\n")
+    
+    subprocess.check_output([
+        "ffmpeg",
+        "-f", "concat",
+        "-safe", "0",
+        "-r", str(framerate),
+        "-i", frame_info_path,
+        "-pix_fmt", "yuv420p",
+        "-movflags", "+faststart",
+        "-vf", f"crop={x1-x0}:{y1-y0}:{x0}:{y0},pad=ceil(iw/2)*2:ceil(ih/2)*2",
+        dst
+    ], stderr=subprocess.DEVNULL)
+
+    os.remove(frame_info_path)
